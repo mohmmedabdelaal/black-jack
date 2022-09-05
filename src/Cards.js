5f6562bbd2a1fbb55d133686a8b0281563362ddd
Moelaal <medo.elaal@gmail.com>
2022-09-05 16:41:35 +0200

    separates cards

---
+++ src/Cards.js
@@ -0,0 +1,31 @@
+import img2 from '../assets/images/2.png';
+import img3 from '../assets/images/3.png';
+import img4 from '../assets/images/4.png';
+import img5 from '../assets/images/5.png';
+import img6 from '../assets/images/6.png';
+import img7 from '../assets/images/7.png';
+import img8 from '../assets/images/8.png';
+import img9 from '../assets/images/9.png';
+import img10 from '../assets/images/10.png';
+import imgJ from '../assets/images/J.png';
+import imgQ from '../assets/images/Q.png';
+import imgA from '../assets/images/A.png';
+import imgK from '../assets/images/K.png';
+
+const Cards = [
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  imgJ,
+  imgQ,
+  imgK,
+  imgA,
+];
+
+export default Cards;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
 import '../assets/css/mdb.min.css';
 import '../assets/css/style.css';
+import theCards from './Cards';
 
-import imgCard from '../assets/images/10.png';
+import hitSound from '../assets/sounds/hit.mp3';
 
 const hitBtn = document.querySelector('#hit-btn');
 const standBtn = document.querySelector('#stand-btn');
 const dealBtn = document.querySelector('#deal-btn');
 
 const info = {
-  human: { scoreSpan: '#human', div: '.human-board', score: 0 },
-  bot: { scoreSpan: '#bot', div: '.bot-board', score: 0 },
+  human: { scoreSpan: '#human', div: '.human-div', score: 0 },
+  bot: { scoreSpan: '#bot', div: '.bot-div', score: 0 },
+  cards: theCards,
+  wins: 0,
+  loses: 0,
+  draws: 0,
 };
 
+separate;
+
+const { human, bot, cards } = info;
+const hSound = new Audio(hitSound);
+
 hitBtn.addEventListener('click', function () {
-  console.log('hit button clicked');
+  const card = pickCard();
+  showCard(card, human);
 });
-standBtn.addEventListener('click', function () {
-  console.log('stand button clicked');
-});
+
 dealBtn.addEventListener('click', function () {
-  console.log('deal button clicked');
+  //   resetCards();
 });
+
+// standBtn.addEventListener('click', function () {
+//   console.log('stand button clicked');
+// });
+const pickCard = () => {
+  return cards[Math.floor(Math.random() * 13)];
+};
+
+const showCard = (card, activePlayer) => {
+  const img = document.createElement('img');
+  img.className = 'col-sm-3 mt-2';
+  img.src = card;
+  document.querySelector(activePlayer.div).appendChild(img);
+  hSound.play();
+};
+
+const resetCards = () => {
+  let allImgs = document.querySelector('.ground').querySelectorAll('img');
+  console.log(allImgs);
+  for (let i = 0; i < allImgs.length; i++) {
+    allImgs[i].remove();
+  }
+};
